32283e168e1d756883b26b3a8227df1c3effc0be
gowtham9344 <gowtham@mobicip.com>
2024-01-08 23:41:37 +0530

    unmodularized code for basic web server with client and server

--- server.c
+++ server.c
@@ -19,8 +19,33 @@
 #define BACKLOG 10 
 #define PORT "8050"
 
+int flag = 0;
 
-// defining functions for post and get send response
+void send_response(int client_socket,const char* status, const char* content_type,const char* content);
+
+
+//send css is used to only the css file.
+void send_css(int client_socket,char fileName[100]){
+	 FILE *file = fopen(fileName, "r");
+	 // open css file
+	 if (file == NULL) {
+		perror("Error opening file");
+		send_response(client_socket, "404 Not Found", "text/plain", "File Not Found");
+		flag = 1;
+		return;
+	 }
+	
+	 //read data from the css file
+	 char buffer[1024];
+	 int bytesRead;
+	 fread(buffer, 1, sizeof(buffer), file);
+		
+	 //send the data to the client
+	 send_response(client_socket, "200 OK", "text/css", buffer);
+    	 fclose(file);
+}
+
+// defining HTTP header and send data to the client
 void send_response(int client_socket,const char* status, const char* content_type,const char* content){
 	char response[2048]; 
 	sprintf(response, "HTTP/1.1 %s\r\nContent-Type: %s\r\n\r\n%s", status, content_type, content);
@@ -28,12 +53,13 @@ void send_response(int client_socket,const char* status, const char* content_typ
 }
 
 // handle saving of data into the file 
-void store_data(int client_socket,char content[1024]){
+void store_data(int client_socket,char content[1024],char fileName[100]){
 	    // Open a file for writing
-	    FILE *file = fopen("output.txt", "a"); // Open in 'a' (append) mode to append new data
+	    FILE *file = fopen(fileName, "a"); // Open in append mode to append new data
 	    if (file == NULL) {
 		perror("Error opening file");
 		send_response(client_socket, "500 Internal Server Error", "text/plain", "Error opening file");
+		flag = 1;
 		return;
 	    }
 
@@ -44,15 +70,18 @@ void store_data(int client_socket,char content[1024]){
 	    fclose(file);
 }
 
-// retrieve all data from the file
-void getalldata(int client_socket,char content[1024]){
+// retrieve all data from the file in the list form
+void getalldata(int client_socket,char content[1024],char fileName[100]){
 	    // Open the file for reading
-	    FILE *file = fopen("output.txt", "r");
+	    FILE *file = fopen(fileName, "r");
 	    if (file == NULL) {
 		perror("Error opening file");
 		send_response(client_socket, "500 Internal Server Error", "text/plain", "Error opening file");
+		flag = 1;
 		return;
 	    }
+
+	    //get data from file
 	    char line[100];
 	    int len = 0;
 	    while(fgets(line,100,file)){
@@ -64,30 +93,42 @@ void getalldata(int client_socket,char content[1024]){
 	    fclose(file);
 }
 
-void handle_get_request(int client_socket) {
+//handling get request without query parameters
+void handle_get_request(int client_socket,char fileName[100]) {
     char buff[1024];
-    getalldata(client_socket,buff); 
+	
+    // get all data from the specified file
+    getalldata(client_socket,buff,fileName); 
+    if(flag == 1)
+	return;
+    
+    //create html content for displaying data
     char html_content[1024];
-    sprintf(html_content,"<html><body>%s</body></html>",buff);
-    // HTML content for browser request
-
+    sprintf(html_content,"<html><head><link rel=\"stylesheet\" href=\"styles.css\"></head><body><ul>%s</ul></body></html>",buff);
+  
     send_response(client_socket, "200 OK", "text/html", html_content);
 }
 
-void handle_post_request(int client_socket, char content[]) {
-    // HTML  content for Post request
+
+// handle post request and get request with query parameters
+void handle_post_request(int client_socket, char content[],char fileName[100]) {
     char* p;
+
+    // replace '=' with ':'
     while((p = strstr(content,"=")) != NULL){
     	*p = ':';
     }
     char response_content[1024];
-    store_data(client_socket,content);
-    sprintf(response_content, "<html><body>Data is posted : </br>%s</body></html>", content);
+    store_data(client_socket,content,fileName);
+    if(flag == 1)
+	return;
+    // HTML  content
+    sprintf(response_content, "<html><head><link rel=\"stylesheet\" href=\"styles.css\"></head><body><h1>Data is posted</h1><p>%s</p></body></html>", content);
     send_response(client_socket, "200 OK", "text/html", response_content);
 }
 
 
-// Function to decode URL-encoded string
+// Function to decode URL-encoded string to normal string
 void url_decode(char *str) {
     int i, j = 0;
     char c;
@@ -107,8 +148,31 @@ void url_decode(char *str) {
     str[j] = '\0';
 }
 
+//parsing of query parameters and store in content array
+void parse_query_parameters(const char *query_string,char content[1024]) {
+    char parameter[50];
+    char value[50];
+    content[0] = '\0';
+    int len = 0;
 
 
+    // Parse query parameters
+    while (sscanf(query_string, "%49[^=]=%49[^&]", parameter, value) == 2) {
+	char line2[100];
+	memset(line2,'\0',sizeof(line2));
+	sprintf(line2,"%s=%s\n",parameter,value);
+        len += strlen(line2);
+	strcat(content,line2);
+	
+        // Move to the next parameter
+        query_string = strchr(query_string, '&');
+        if (query_string == NULL) {
+            break;
+        }
+        query_string++; // Skip the '&'
+    }
+    content[len-1] = '\0';
+}
 
 
 void sigchld_handler(int s){
@@ -210,12 +274,53 @@ int main(){
 			buff[c] = '\0';
  
  			char method[10];
+			char fileName[100] = "output.txt";
+			char route[100];
+			char queryData[1024];
+			int query = 0;
 			printf("%s",buff);
-        		sscanf(buff, "%s", method);
-						
+        		sscanf(buff, "%s /%s", method,route);
+				
+
+			if(strcmp(route,"HTTP/1.1")){
+				char* queryPointer = strstr(route,"?");
+				if(queryPointer == NULL){
+					strcpy(fileName,route);
+				}
+				else{
+					sscanf(route, "%[^?]s", fileName);
+					query = 1;
+					if (queryPointer != NULL) {
+						// Move to the actual query string
+						queryPointer++;
+
+						// Parse query parameters
+						parse_query_parameters(queryPointer,queryData);
+					 }
+				
+				}
+			}
+			else if(strcmp(route,"?")==0){
+				char* queryPointer = strstr(route,"?");
+				query = 1;
+				if (queryPointer != NULL) {
+					// Move to the actual query string
+					queryPointer++;
+
+					// Parse query parameters
+					parse_query_parameters(queryPointer,queryData);
+				}
+			}
 			
-			if(strcmp(method,"GET") == 0){
-				handle_get_request(connfd);			
+					
+			if(strstr(route,".css") != NULL){
+				send_css(connfd,fileName);	
+			}
+			else if(strcmp(method,"GET") == 0 && query == 1){
+				handle_post_request(connfd,queryData,fileName);			
+			}			
+			else if(strcmp(method,"GET") == 0){
+				handle_get_request(connfd,fileName);			
 			}else if(strcmp(method,"POST") == 0){			
 				char* content_ptr = strstr(buff,"Content-Length:");	
 				if(content_ptr != NULL){
@@ -227,7 +332,7 @@ int main(){
 					sscanf(content_ptr, "\r\n\r\n%[^\n]s", content);
 					content[content_length] = '\0';
 					url_decode(content);
-					handle_post_request(connfd, content);
+					handle_post_request(connfd, content,fileName);
 				}
 				else {
 					// Handle POST request without Content-Length header
